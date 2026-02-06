5be4e5809a6893819a83b9f508433a36880fc099
Axel Cella <160883113+DoomZyx@users.noreply.github.com>
2026-02-06 14:13:15 +0100

    fix: suppression du prefixe

--- app.js
+++ app.js
@@ -196,7 +196,7 @@ fastify.register(adminOrdersRoutes);
 fastify.register(adminInvoicesRoutes);
 fastify.register(adminUploadRoutes);
 fastify.register(contactRoutes);
-}, { prefix: '/api' });
+});
 
 // Initialiser la connexion à la base de données
 await connectDB();
